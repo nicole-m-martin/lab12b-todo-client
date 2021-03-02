6d0616f261d5285e0c8e3b26912af5fa7711370c
Nicole Martin <n.martinpdx@gmail.com>
2021-03-02 15:51:35 -0800

    fix routs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-
+import React, { Component } from 'react'
 import './App.css';
+import {
+    BrowserRouter as Router, 
+    Route, 
+    Switch,
+} from 'react-router-dom';
+import Header from './Components/HeaderPage.js';
+import PrivateRoute from './AuthPages/PrivateRoute.js';
+import HomePage from './Components/HomePage.js';
+import SignUpPage from './AuthPages/SignUpPage.js';
+import LoginPage from './AuthPages/LoginPage.js';
+import TodoListPage from './Components/TodoListPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
-    </div>
-  );
+
+
+  export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <Router>
+                    <Header />
+                    <Switch>
+                        <Route 
+                            path="/" 
+                            exact
+                            component={HomePage} 
+                        />
+                        <PrivateRoute 
+                            path="/todo" 
+                            exact
+                            component={TodoListPage} 
+                        />            
+                        <Route 
+                            path="/login" 
+                            exact
+                            component={LoginPage} 
+                        />
+                        <Route 
+                            path="/signup" 
+                            exact
+                            component={SignUpPage} 
+                        />
+                    </Switch>
+                </Router>
+            </div>
+        )
+    }
 }
-
-export default App;
--- src/Components/HeaderPage.js
+++ src/Components/HeaderPage.js
@@ -1,49 +1,11 @@
 import React, { Component } from 'react'
-import './App.css';
-import {
-    BrowserRouter as Router, 
-    Route, 
-    Switch,
-} from 'react-router-dom';
-import Header from '../Components/HeaderPage.js';
-import PrivateRoute from '../AuthPages/PrivateRoute.js';
-import HomePage from '../Components/HomePage.js';
-import SignUpPage from '../AuthPages/SignUpPage.js';
-import LoginPage from '../AuthPages/LoginPage.js';
-import TodoListPage from '../Components/TodoListPage';
 
-
-
-  export default class App extends Component {
-    render() {
-        return (
-            <div>
-                <Router>
-                    <Header />
-                    <Switch>
-                        <Route 
-                            path="/" 
-                            exact
-                            component={HomePage} 
-                        />
-                        <PrivateRoute 
-                            path="/todo" 
-                            exact
-                            component={TodoListPage} 
-                        />            
-                        <Route 
-                            path="/login" 
-                            exact
-                            component={LoginPage} 
-                        />
-                        <Route 
-                            path="/signup" 
-                            exact
-                            component={SignUpPage} 
-                        />
-                    </Switch>
-                </Router>
-            </div>
-        )
-    }
+export default class HeaderPage extends Component {
+  render() {
+    return (
+      <div>
+        header
+      </div>
+    )
+  }
 }
